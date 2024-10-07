90c6e2eca5d1d5dcf5952c298e48bbbf02d74c9d
alexlop29 <alexander.lopez@owasp.org>
2024-10-07 17:12:59 -0400

    Add diff states and accessibility add-on

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ### Notes
 > Storybook is a frontend workshop for building UI components and pages in isolation.
 
+> Storybook includes an official accessibility addon. Powered by Deque's axe-core, it can catch up to 57% of WCAG issues: yarn add --dev @storybook/addon-a11y
+
 ### Definitions
 > Component-Driven Development (CDD) methodology: A process that builds UIs from the “bottom-up”, starting with components and ending with screens
 
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -6,22 +6,57 @@ type Task = {
 
 type InputProps = {
   task: Task;
-  onArchiveTask: () => void;
-  onPinTask: () => void;
+  onArchiveTask: (id: Task["id"]) => void;
+  onPinTask: (id: Task["id"]) => void;
 };
 
 export const Task = ({ task, onArchiveTask, onPinTask }: InputProps) => {
   return (
     <div className="list-item">
-      <label htmlFor={`title-${task.id}`} aria-label={task.title}>
+      <label
+        htmlFor={`archiveTask-${task.id}`}
+        aria-lebl={`archiveTask-${task.id}`}
+        className="checkbox"
+      >
+        <input
+          type="checkbox"
+          disabled={true}
+          name="checked"
+          id={`archiveTask-${task.id}`}
+          checked={task.state === "TASK_ARCHIVED"}
+        />
+        <span
+          className="checkbox-custom"
+          onClick={() => onArchiveTask(task.id)}
+        />
+      </label>
+
+      <label
+        htmlFor={`title-${task.id}`}
+        aria-label={task.title}
+        className="title"
+      >
         <input
           type="text"
           value={task.title}
           readOnly={true}
           name="title"
           id={`title-${task.id}`}
+          placeholder="Input title"
         />
       </label>
+
+      {task.state !== "TASK_ARCHIVED" && (
+        <button
+          className="pin-button"
+          onClick={() => onPinTask(task.id)}
+          id={`pintask-${task.id}`}
+          aria-label={`pintask-${task.id}`}
+          key={`pintask-${task.id}`}
+        >
+          <span className={`icon-star`} />
+        </button>
+      )}
     </div>
   );
 };
